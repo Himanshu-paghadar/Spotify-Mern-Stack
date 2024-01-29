875a7c543a66713d110da4403065fe0483132449
Himanshu Paghadar <Hpaghadar85@gmail.com>
2024-01-30 00:25:34 +0530

    User backend Completed

---
+++ spotify_frontend/src/routes/Login.js
@@ -0,0 +1,14 @@
+import { Icon } from "@iconify/react";
+const LoginComponent = () => {
+	return (
+		<div className="w-full h-full flex flex-col items-center ">
+			<div className="logo p-5 border-b border-solid w-full border-gray-300 flex justify-center">
+				<Icon icon="logos:spotify" width="140" />
+            </div>
+            <div className="InputRegion">
+
+            </div>
+		</div>
+	);
+};
+export default LoginComponent;
