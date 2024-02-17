fc257b023b316548e3c33a2dffdd3bc8f4891441
Himanshu Paghadar <Hpaghadar85@gmail.com>
2024-02-17 11:48:29 +0530

    Added private auth routes

--- spotify_frontend/src/App.js
+++ spotify_frontend/src/App.js
@@ -3,16 +3,26 @@ import "./output.css";
 import LoginComponent from "./routes/Login";
 import SignupComponent from "./routes/Signup";
 import HomeComponent from "./routes/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 function App() {
+	const [cookies, setCookies] = useCookies(["token"]);
 	return (
 		<div className="w-screen h-screen font-poppins">
 			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<HomeComponent />} />
-					<Route path="/login" element={<LoginComponent />} />
-					<Route path="/signup" element={<SignupComponent />} />
-				</Routes>
+				{cookies.token ? (
+					<Routes>
+						<Route path="/" element={<HomeComponent />} />
+						<Route path="*" element={<Navigate to="/login" />} />
+					</Routes>
+				) : (
+					<Routes>
+						<Route path="/" element={<HomeComponent />} />
+						<Route path="/login" element={<LoginComponent />} />
+						<Route path="/signup" element={<SignupComponent />} />
+						<Route path="*" element={<Navigate to="/login" />} />
+					</Routes>
+				)}
 			</BrowserRouter>
 		</div>
 	);
--- spotify_frontend/src/routes/Signup.js
+++ spotify_frontend/src/routes/Signup.js
@@ -1,7 +1,8 @@
 import { Icon } from "@iconify/react";
 import TextInput from "../components/shared/TextInput";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useCookies } from "react-cookie";
 import { makeUnauthenticatedPOSTRequest } from "../utils/serverHelpers";
 
 const SignupComponent = () => {
@@ -11,6 +12,8 @@ const SignupComponent = () => {
 	const [password, setPassword] = useState("");
 	const [firstName, setFirstName] = useState("");
 	const [lastName, setLastName] = useState("");
+	const [cookies, setCookies] = useCookies(["token"]);
+	const navigate = useNavigate();
 
 	const signUp = async () => {
 		if (email !== confirmEmail) {
@@ -23,8 +26,12 @@ const SignupComponent = () => {
 			data
 		);
 		if (response && !response.err) {
-			console.log(response);
+			const token = response.token;
+			const date = new Date();
+			date.setDate(date.getDate() + 30);
+			setCookies("token", token, { path: "/", expires: date });
 			alert("Success");
+			navigate("/login");
 		} else {
 			alert("Fail");
 		}
@@ -35,67 +42,69 @@ const SignupComponent = () => {
 				<Icon icon="logos:spotify" width="140" />
 			</div>
 			<div className="InputRegion sm:w-2/3 md:w-1/2 lg:w-1/3 py-8 flex flex-col justify-center items-center">
-				<div className="font-bold mb-5 text-xl">
-					Signup up for free to start listening.
-				</div>
+				<form>
+					<div className="font-bold mb-5 text-xl">
+						Signup up for free to start listening.
+					</div>
 
-				<TextInput
-					type="email"
-					label="What's your email?"
-					placeholder="Enter your email."
-					value={email}
-					setValue={setEmail}
-				/>
-				<TextInput
-					type="email"
-					label="Confirm your email"
-					placeholder="Enter your email again."
-					value={confirmEmail}
-					setValue={setConfirmEmail}
-				/>
-				<TextInput
-					type="text"
-					label="Create a username"
-					placeholder="Create a username."
-					value={username}
-					setValue={setUsername}
-				/>
-				<TextInput
-					type="password"
-					label="Create a password"
-					placeholder="Create a strong password."
-					value={password}
-					setValue={setPassword}
-				/>
-				<div className="w-full flex flex-col md:flex-row justify-between items-center">
 					<TextInput
-						type="text"
-						label="Your's First Name"
-						placeholder="Enter your firstname."
-						className="mb-4 pr-2 md:mb-0 md:mr-2 w-full md:w-1/2 lg:w-1/2"
-						value={firstName}
-						setValue={setFirstName}
+						type="email"
+						label="What's your email?"
+						placeholder="Enter your email."
+						value={email}
+						setValue={setEmail}
+					/>
+					<TextInput
+						type="email"
+						label="Confirm your email"
+						placeholder="Enter your email again."
+						value={confirmEmail}
+						setValue={setConfirmEmail}
 					/>
 					<TextInput
 						type="text"
-						label="Your's Last Name"
-						placeholder="Enter your lastname."
-						className="mb-4 md:mb-0 md:mr-2 w-full md:w-1/2 lg:w-1/2"
-						value={lastName}
-						setValue={setLastName}
+						label="Create a username"
+						placeholder="Create a username."
+						value={username}
+						setValue={setUsername}
 					/>
-				</div>
-				<div className="w-full flex justify-center my-7">
-					<button
-						className="bg-spotify1 hover:text-white p-2 px-5 rounded-full font-semibold transform transition-transform hover:scale-105"
-						onClick={(e) => {
-							e.preventDefault();
-							signUp();
-						}}
-					>
-						SIGN UP
-					</button>
-				</div>
+					<TextInput
+						type="password"
+						label="Create a password"
+						placeholder="Create a strong password."
+						value={password}
+						setValue={setPassword}
+					/>
+					<div className="w-full flex flex-col md:flex-row justify-between items-center space-x-3">
+						<TextInput
+							type="text"
+							label="Your's First Name"
+							placeholder="Enter your firstname."
+							className="mb-4 md:mb-0 md:mr-2 w-full md:w-1/2 lg:w-1/2"
+							value={firstName}
+							setValue={setFirstName}
+						/>
+						<TextInput
+							type="text"
+							label="Your's Last Name"
+							placeholder="Enter your lastname."
+							className="mb-4 md:mb-0 w-full md:w-1/2 lg:w-1/2"
+							value={lastName}
+							setValue={setLastName}
+						/>
+					</div>
+					<div className="w-full flex justify-center my-7">
+						<button
+							className="bg-spotify1 hover:text-white p-2 px-5 rounded-full font-semibold transform transition-transform hover:scale-105"
+							onClick={(e) => {
+								e.preventDefault();
+								signUp();
+							}}
+						>
+							SIGN UP
+						</button>
+					</div>
+				</form>
 				<div className="horizontalLine border border-solid w-full border-gray-300"></div>
 				<div className="font-semibold text-lg my-5">
 					Already have an account ?
